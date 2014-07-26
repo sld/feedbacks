bd6d8174924853caba8e1ace830314d20dda4a2e
Idris <sld7700@gmail.com>
2014-07-26 20:56:42 +0400

    Updated README, added en.yml, deleted rails version restriction.
    Also changed :match to :get route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Feedbacks::Engine.routes.draw do
   # Old api support
-  match "feedback" => "feedback_sender#new"
+  get "feedback" => "feedback_sender#new"
   post "feedback_send" => "feedback_sender#create"
 
   resources :feedback_sender, :only => [:new, :create]
